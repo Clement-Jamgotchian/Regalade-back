999a6e0e7f4308a6f460be53eac025584ca89a48
Corentin-Martin <corentin.mrtn@yahoo.com>
2023-06-06 12:32:39 +0200

    create fixtures for Category, Recipe and ContainsIngredient

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\ContainsIngredient;
 use App\Entity\Department;
 use App\Entity\Ingredient;
+use App\Entity\Recipe;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -14,7 +17,7 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
-        $faker = \Faker\Factory::create();
+        $faker = \Faker\Factory::create('fr_FR');
         $faker->addProvider(new \FakerRestaurant\Provider\fr_FR\Restaurant($faker));
 
         $departments = ["Frais", "Fruits et légumes", "Boucherie"];
@@ -30,6 +33,8 @@ class AppFixtures extends Fixture
 
         } 
 
+        $allIngredients = [];
+
         for ($i=0; $i < 5; $i++) { 
             $ingredient = new Ingredient();
             $ingredient->setName($faker->unique()->dairyName());
@@ -37,6 +42,8 @@ class AppFixtures extends Fixture
             $ingredient->setDepartment($allDepartments[0]);
             $ingredient->setIsCold(1);
             $manager->persist($ingredient);
+
+            $allIngredients[] = $ingredient;
         }
 
         for ($i=0; $i < 15; $i++) { 
@@ -46,6 +53,8 @@ class AppFixtures extends Fixture
             $ingredient->setDepartment($allDepartments[1]);
             $ingredient->setIsCold(0);
             $manager->persist($ingredient);
+
+            $allIngredients[] = $ingredient;
         }
 
         for ($i=0; $i < 4; $i++) { 
@@ -55,6 +64,48 @@ class AppFixtures extends Fixture
             $ingredient->setDepartment($allDepartments[2]);
             $ingredient->setIsCold(1);
             $manager->persist($ingredient);
+        }
+
+        $categories = ['Entrée', "Plat", "Dessert"];
+
+        $allCategories = [];
+        foreach ($categories as $category) {
+            $newCategory = new Category();
+            $newCategory->setTitle($category);
+            $manager->persist($newCategory);
+
+            $allCategories[] = $newCategory;
+
+        }
+
+        $allRecipes = [];
+
+        for ($i=0; $i < 11; $i++) { 
+            $newRecipe = new Recipe();
+            $newRecipe->setTitle($faker->unique()->foodName());
+            $newRecipe->setDescription($faker->realText(50));
+            $newRecipe->setCookingDuration($faker->randomNumber(2));
+            $newRecipe->setSetupDuration($faker->randomNumber(2));
+            $newRecipe->setStep($faker->realText(250));
+            $newRecipe->setDifficulty(mt_rand(1,3));
+            $manager->persist($newRecipe);
+
+            $allRecipes[] = $newRecipe;
+        }
+
+        foreach ($allRecipes as $recipe) {
+
+            $randomNb = mt_rand(2, 6);
+            for ($i=1; $i <= $randomNb; $i++) {
+
+                $newContainsIngredient = new ContainsIngredient();
+                $newContainsIngredient->setQuantity(mt_rand(1, 500));
+                $newContainsIngredient->setRecipe($recipe);
+                $newContainsIngredient->setIngredient($allIngredients[mt_rand(0, count($allIngredients)-1)]);
+
+                $manager->persist($newContainsIngredient);
+
+            }
         }
 
         $manager->flush();
