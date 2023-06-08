fb191f922ac57425ba530858688f52267c633f1c
Corentin-Martin <corentin.mrtn@yahoo.com>
2023-06-08 13:25:50 +0200

    purge old list in function add + create function delete for CartController

--- src/Controller/Api/CartController.php
+++ src/Controller/Api/CartController.php
@@ -5,11 +5,13 @@ namespace App\Controller\Api;
 use App\Entity\Cart;
 use App\Entity\User;
 use App\Repository\CartRepository;
+use App\Repository\UserRepository;
 use App\Services\UserService;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -20,10 +22,16 @@ class CartController extends AbstractController
     /**
      * @Route("", name="add", methods={"POST"})
      */
-    public function add(UserService $userService, EntityManagerInterface $entityManagerInterface): JsonResponse
+    public function add(UserService $userService, CartRepository $cartRepository, EntityManagerInterface $entityManagerInterface): JsonResponse
     {
         /** @var User */
         $user = $userService->getCurrentUser();
+
+        // Before add, purge old list
+        $cart = $user->getCarts();
+        foreach ($cart as $cartElement) {
+            $cartRepository->remove($cartElement);
+        }
 
         $recipesList = $user->getRecipe();
 
@@ -46,5 +54,24 @@ class CartController extends AbstractController
         $entityManagerInterface->flush();
 
         return $this->json($allCart, 200, [], ['groups' => ["ingredient_browse", "cart_browse"]]);
+    }
+
+    /**
+     * @Route("", name="delete", methods={"DELETE"})
+     */
+    public function delete(UserService $userService, CartRepository $cartRepository, EntityManagerInterface $entityManagerInterface): JsonResponse
+    {
+        /** @var User */
+        $user = $userService->getCurrentUser();
+
+        $cart = $user->getCarts();
+
+        foreach ($cart as $cartElement) {
+            $cartRepository->remove($cartElement);
+        }
+
+        $entityManagerInterface->flush();
+
+        return $this->json(["message" => "Tous les ingrédients ont été supprimé du panier"], Response::HTTP_OK);
     }
 }
