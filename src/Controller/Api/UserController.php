78f2392257a6e649b3d8820e6b977a4bd5753c94
Corentin-Martin <corentin.mrtn@yahoo.com>
2023-06-16 14:21:11 +0200

    put for edit user infos

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 /**
@@ -41,6 +42,21 @@ class UserController extends AbstractController
     public function read(): JsonResponse
     {
         $user = $this->getUser();
+
+        return $this->json($user, Response::HTTP_OK, [], ["groups" => ["user_browse"]]);
+    }
+
+    /**
+     * @Route("", name="edit", methods={"PUT", "PATCH"})
+     */
+    public function edit(Request $request, SerializerInterface $serializerInterface, UserPasswordHasherInterface $userPasswordHasherInterface, UserRepository $userRepository): JsonResponse
+    {
+        /** @var User */
+        $user = $serializerInterface->deserialize($request->getContent(), User::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $this->getUser()]);
+
+        $user->setPassword($userPasswordHasherInterface->hashPassword($user, $user->getPassword()));
+
+        $userRepository->add($user, true);
 
         return $this->json($user, Response::HTTP_OK, [], ["groups" => ["user_browse"]]);
     }
