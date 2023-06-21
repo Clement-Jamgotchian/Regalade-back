8539219e4139dc1de9e7d6edee6cf10ddedec191
GaelDeCecco <gael.de-cecco@wanadoo.fr>
2023-06-21 12:52:29 +0200

    edit + create user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -12,62 +12,55 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        /** @var User $user */
         $builder
-                ->add('email', EmailType::class, [
-                    "label" => "Email pour se logger"
-                ])
-                ->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
-                    $builder = $event->getForm();
-                    /** @var User $user */
-                    $user = $event->getData();
-                    if ($user->getId() !== null) {
-                        $builder
-                            ->add('password', PasswordType::class, [
-                              "mapped" => false,
-                               "label" => "le mot de passe",
-                                "attr" => [
-                                    "placeholder" => "laisser vide pour ne pas modifier ..."
-                                ],
-                            ])
-                            ->add('roles', ChoiceType::class, [
-                                "multiple" => true,
-                                "expanded" => true,
-                                "choices" => [
-                                    "ADMIN" => "ROLE_ADMIN",
-                                    "USER" => "ROLE_USER",
-                                ]
-                            ])    
-                            ->add('nickname')
-                            ->add('picture');
-                
-                    } else {
-                        $builder
-                        ->add('password', PasswordType::class, [
-                            "mapped" => false,
-                             "label" => "le mot de passe",
-                              "attr" => [
-                                  "placeholder" => "laisser vide pour ne pas modifier ..."
-                              ],
-                          ])
-                            ->add('roles', ChoiceType::class, [
-                                "multiple" => true,
-                                "expanded" => true,
-                                "choices" => [
-                                    "ADMIN" => "ROLE_ADMIN",
-                                    "USER" => "ROLE_USER",
-                                ]
-                            ])
-    
-                            ->add('nickname')
-                            ->add('picture');
-                        }
-                    });
+            ->add('email')
+            ->add('nickname')
+            ->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
+                $builder = $event->getForm();
+                $user = $event->getData();
+
+                if ($user->getId() === null) {
+                    $builder->add('password', null, [
+                        'empty_data' => '',
+                        'constraints' => [
+                            new NotBlank(),
+                        ],
+                            
+                    ]);
                 }
+            })
+            ->add('roles', ChoiceType::class, [
+                "multiple" => true,
+                "expanded" => true,
+                "choices" => [
+                    "ADMIN" => "ROLE_ADMIN",
+                    "USER" => "ROLE_USER",
+                ]
+            ])
+        ;
+    }
+
+    //     $builder
+    //             ->add('email')
+    //             ->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
+    //                 $builder = $event->getForm();                    
+    //             })
+    //             ->add('roles', ChoiceType::class, [
+    //                 "multiple" => true,
+    //                 "expanded" => true,
+    //                 "choices" => [
+    //                     "ADMIN" => "ROLE_ADMIN",
+    //                     "USER" => "ROLE_USER"
+    //                 ]
+    //             ]);
+    // }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
